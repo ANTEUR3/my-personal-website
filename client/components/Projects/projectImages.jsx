2f08946144807cb0a6a243a36aef25a9b3fd9d54
ANTEUR3 <anteuryounes1@gmail.com>
2024-12-30 03:25:51 +0100

    add nice design to the product details page

--- client/components/Projects/projectImages.jsx
+++ client/components/Projects/projectImages.jsx
@@ -5,7 +5,7 @@ export const ProjectImages = ({Images,projectId,setImage}) => {
 
  const projectImages=useMemo(()=>{
   return Images.map((Img,key)=>{
-    return <img onClick={()=>{setImage(Img)}} src={`/images/${projectId}/${Img.id}.png`} className='lg:w-[95%] lg:h-[130px] border-2 rounded-lg border-gray-800 cursor-pointer' />
+    return <img key={key} onClick={()=>{setImage(Img)}} src={`/images/${projectId}/${Img.id}.png`} className='lg:w-[95%] lg:h-[130px] border-2 rounded-lg border-gray-800 cursor-pointer' />
   }
 )
  },[Images])
@@ -15,7 +15,7 @@ useEffect(()=>{
 },[Images,projectId])
     
   return (
-    <div className='w-[20%] flex flex-col justify-start items-start   gap-y-3 lg:pl-[30px]   lg:h-[430px] overflow-scroll'>
+    <div className='relative z-19 w-[20%] flex flex-col justify-start items-start   gap-y-3 lg:pl-[30px]   lg:h-[430px] overflow-scroll'>
      
       {Images.length>0?projectImages:'no'}
     </div>
--- client/pages/Projects/[project].js
+++ client/pages/Projects/[project].js
@@ -4,6 +4,8 @@ import { appContext } from '@/DataContext.js';
 import WaitingData from '@/components/WaitingData';
 import ProjectDetails from '@/components/Projects/ProjectDetails';
 import { ProjectImages } from '@/components/Projects/projectImages';
+import { FaHandPointUp } from "react-icons/fa";
+
 const Project = () => {
     const [projectDisplay, setProjectDisplay] = useState(null);
     const [skills,setSkills]=useState([]);
@@ -33,27 +35,34 @@ const Project = () => {
  
 
     return (
-        <div className=' p-4 bg-gray-100 lg:pt-[100px] w-full h-screen overflow-hidden   '>
-            {projectDisplay ? (
-                <>
-                <div className='h-screen w-full px-3 flex justify-start items-start'>
+<div className=' p-4 bg-gray-100 lg:pt-[100px] w-full h-screen overflow-hidden   '>
+            {projectDisplay ? 
+                <div >
+                
+                
+                <div className='h-screen w-full px-3 flex justify-start items-start   z-100 relative'>
                   <ProjectDetails project={projectDisplay} projectSkills={skills} Images={Images} Image={Image}/>
                   <ProjectImages   Images={Images} projectId={projectId} setImage={setImage} />
 
                 </div>
-                <div className={`absolute lg:w-[600px] lg:py-[20px] border-2 rounded-xl shadow-2xl shadow-slate-700 bg-gray-100 lg:left-[110px] top-[50%]  flex flex-col justify-center items-center gap-y-4 ${Image==null?'-translate-y-[800px]  ':' -translate-y-[50%]'}`}>
-                {Image!=null? <img src={`/images/${projectId}/${Image.id}.png`} className={`lg:w-[400px] h-[300px] rounded-lg `} />:''}
+                     <div className={` absolute lg:w-[600px] lg:py-[20px] border-2 rounded-xl shadow-2xl shadow-slate-700 bg-gray-100   flex flex-col justify-center items-center gap-y-4 transition-transform duration-1000  top-[50%] left-[10%] ${Image==null?'-translate-y-[800px]  ':' -translate-y-[40%]'}`}>
+                          {Image!=null? <img src={`/images/${projectId}/${Image.id}.png`} className={`lg:w-[400px] h-[300px] rounded-lg `} />:''}
                   
-                  <p>aaaaaaaaaaaaaaaaaaaaaaa</p>
+                          <p>aaaaaaaaaaaaaaaaaaaaaaa</p>
+<FaHandPointUp onClick={()=>{setImage(null)}} className='absolute -top-10 left-1/2 -ranslate-x-[50%] text-5xl cursor-pointer hover:text-red-600'/>
+
+                    </div>
+
                 </div>
+               
                    
-                </>
                 
-            ) : (
+             : 
                 <WaitingData />
-            )}
+            }
         </div>
     )
+    
 }
 
-export default Project
+export default Project;
